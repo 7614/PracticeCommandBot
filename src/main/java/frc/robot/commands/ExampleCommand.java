8659991603b58918fbea0a3ee410b59da16edbca
lukeKlassen <7614lukeKlassen@users.noreply.github.com>
2018-11-15 23:09:39 -0500

    Messed Around Some More
    then went to bed.

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -27,6 +27,7 @@ public class ExampleCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    System.out.println("Let Me Live / Let Me Die");
   }
 
   // Make this return true when this Command no longer needs to run execute()
